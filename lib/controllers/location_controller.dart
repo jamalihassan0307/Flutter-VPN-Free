436db961c1798b11b9c8e64252637310b993a82a
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-03-15 21:51:44 +0500

    Add Japan flag and OpenVPN configuration: Introduce a new flag image for Japan and update the Japan OpenVPN configuration file. Modify LocationController to include Japan in the VPN list with relevant details, enhancing the VPN selection options.

--- lib/controllers/location_controller.dart
+++ lib/controllers/location_controller.dart
@@ -24,59 +24,86 @@ class LocationController extends GetxController {
 
   List<Vpn> generateVpnList() {
     final List<Map<String, String>> countryData = [
-      {'country': 'Ecuador', 'short': 'EC', 'image': 'assets/flags/ec.png', 'config': 'assets/vpn/Ecuador.ovpn'},
-      {'country': 'Romania', 'short': 'RO', 'image': 'assets/flags/ro.png', 'config': 'assets/vpn/Romania.ovpn'},
       {
+        'ipAddress': '219.100.37.56',
+        'hostname': 'public-vpn-94',
+        'country': 'Ecuador',
+        'short': 'EC',
+        'image': 'assets/flags/ec.png',
+        'config': 'assets/vpn/Ecuador.ovpn'
+      },
+      {
+        'ipAddress': '219.100.37.56',
+        'hostname': 'public-vpn-94',
+        'country': 'Romania',
+        'short': 'RO',
+        'image': 'assets/flags/ro.png',
+        'config': 'assets/vpn/Romania.ovpn'
+      },
+      {
+        'ipAddress': '219.100.37.56',
+        'hostname': 'public-vpn-94',
         'country': 'Russian Federation',
         'short': 'RU',
         'image': 'assets/flags/ru.png',
         'config': 'assets/vpn/Russian Federation.ovpn'
       },
-      {'country': 'Taiwan', 'short': 'TW', 'image': 'assets/flags/tw.png', 'config': 'assets/vpn/Taiwan.ovpn'},
       {
+        'ipAddress': '219.100.37.56',
+        'hostname': 'public-vpn-94',
+        'country': 'Taiwan',
+        'short': 'TW',
+        'image': 'assets/flags/tw.png',
+        'config': 'assets/vpn/Taiwan.ovpn'
+      },
+      {
+        'ipAddress': '219.100.37.56',
+        'hostname': 'public-vpn-94',
         'country': 'United States',
         'short': 'US',
         'image': 'assets/flags/us.png',
         'config': 'assets/vpn/United States.ovpn'
       },
-      {'country': 'Viet Nam', 'short': 'VN', 'image': 'assets/flags/vn.png', 'config': 'assets/vpn/Viet Nam.ovpn'},
       {
+        'ipAddress': '219.100.37.56',
+        'hostname': 'public-vpn-94',
+        'country': 'Viet Nam',
+        'short': 'VN',
+        'image': 'assets/flags/vn.png',
+        'config': 'assets/vpn/Viet Nam.ovpn'
+      },
+      {
+        'ipAddress': '14.36.22.137',
+        'hostname': 'vpn692884786.opengw.net',
         'country': 'Korea Republic of',
         'short': 'KR',
         'image': 'assets/flags/kr.png',
         'config': 'assets/vpn/Korea Republic of.ovpn'
       },
-      {'country': 'Indonesia', 'short': 'ID', 'image': 'assets/flags/id.png', 'config': 'assets/vpn/Indonesia.ovpn'},
-    ];
-
-    final List<String> ipAddresses = [
-      '219.100.37.56',
-      '203.160.64.70',
-      '178.32.221.21',
-      '45.77.56.114',
-      '139.162.123.45',
-      '108.61.201.119',
-      '185.162.235.3',
-      '103.86.99.10'
-    ];
-
-    final List<String> hostnames = [
-      'public-vpn-94',
-      'public-vpn-95',
-      'public-vpn-96',
-      'public-vpn-97',
-      'public-vpn-98',
-      'public-vpn-99',
-      'public-vpn-100',
-      'public-vpn-101'
+      {
+        'ipAddress': '219.100.37.56',
+        'hostname': 'public-vpn-94',
+        'country': 'Indonesia',
+        'short': 'ID',
+        'image': 'assets/flags/id.png',
+        'config': 'assets/vpn/Indonesia.ovpn'
+      },
+      {
+        'ipAddress': '219.100.37.126',
+        'hostname': 'public-vpn-163.opengw.net',
+        'country': 'Japan',
+        'short': 'japan',
+        'image': 'assets/flags/japan.png',
+        'config': 'assets/vpn/japan.ovpn'
+      },
     ];
 
     List<Vpn> vpnList = [];
     for (int i = 0; i < countryData.length; i++) {
       final country = countryData[i];
       vpnList.add(Vpn(
-        hostname: hostnames[i],
-        ip: ipAddresses[i],
+        hostname: country['hostname']!,
+        ip: country['ipAddress']!,
         countryLong: country['country']!,
         countryShort: country['short']!,
         imagePath: country['image']!,
